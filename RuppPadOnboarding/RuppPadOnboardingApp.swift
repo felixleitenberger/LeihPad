40f6db49ed13b89ec14751b0fec7e83ef0d99b42
crabucate <felix.leitenberger@gmail.com>
2023-10-21 14:53:43 +0200

    Initial Commit

---
+++ RuppPadOnboarding/RuppPadOnboardingApp.swift
@@ -0,0 +1,17 @@
+//
+//  RuppPadOnboardingApp.swift
+//  RuppPadOnboarding
+//
+//  Created by Felix Leitenberger on 21.10.23.
+//
+
+import SwiftUI
+
+@main
+struct RuppPadOnboardingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
